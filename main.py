74c010225b3b046dca203aad12c644d5fb256807
Shootsoo <jacoreydotson1@gmail.com>
2024-11-21 09:36:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+def pizza_price():
 
+
+  requsted_toppings = input(""")
+
+  requsted_toppings = requsted_toppings.upper()
+  print(f"You requsted: {requsted_toppings}")
+
+  base_price = 15
+  toppings_price = 0
+  calculated_toppings = []
+
+  for topping in requsted_toppings:
+
+  if topping in calculated_toppings:
+      continue
